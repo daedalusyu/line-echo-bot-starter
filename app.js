59ca36327abd52f5dfc02ae415030e383199adef
daedalusyu <daedalusyu@users.noreply.github.com>
2017-09-03 15:02:25 +0800

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let bodyParser = require('body-parser')
 let request = require('request')
 let app = express()
 
-const CHANNEL_ACCESS_TOKEN = 'Your_Channel_Access_Token'
+const CHANNEL_ACCESS_TOKEN = 'Q8Ld7GL/KST112ryzYWXjZSuIziHkcPIk2YIx3tquQE6Y7SKGJ9khHkAciluuY2tfFADEEaTzHHAmEEU+Ne9Vttq90xeFYSKMTE3DcM4cll16LP7HX222o3BCg1PZ3z7i26WxVa83TD1l2LW9W+FUwdB04t89/1O/w1cDnyilFU='
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,6 +14,13 @@ app.listen(PORT, function () {
 
 // handler receiving messages
 app.post('/', function (req, res) {
+    console.log(JSON.stringify(req.body, null, 2));
+    let replyToken = req.body.events[0].replyToken;
+    let text = req.body.events[0].message.text;
+    if (text) {
+        sendMessage(replyToken, text);
+    }
+    res.send();
 })
 
 // generic function sending messages
